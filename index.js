cf5e2d5288a9c20948aa67ad1b700a584b36297a
Hannah Christmann <hc109909@gmail.com>
2022-08-26 12:32:52 -0400

    commit tree 05aa188f31bbcaf79ec4aaeac4c578dfbc482446

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    let hqLocation = 42
+    return Math.abs(hqLocation - pickupLocation)
+}
+
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks(pickupLocation) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start - destination) * 264)
+
+}
